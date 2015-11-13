664b15f4fc7314993b0cecd224eea3130c9a1dbe
Rick <prefect12@gmail.com>
2015-11-13 01:40:27 -0800

    Initial Commit.
    _Layout.cshtml functional.

---
+++ rthowell.net (ASP MVC)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="rthowell.net__ASP_MVC_.MvcApplication" Language="C#" %>
---
+++ rthowell.net (ASP MVC)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rthowell.net__ASP_MVC_.Startup))]
+namespace rthowell.net__ASP_MVC_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
